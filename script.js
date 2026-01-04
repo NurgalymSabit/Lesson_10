9a6a207a6e461a337346b469bc39a3f751f8b011
Sabitovv <166857532+Sabitovv@users.noreply.github.com>
2026-01-04 14:46:30 +0500

    Accardion

--- script.js
+++ script.js
@@ -33,3 +33,37 @@ function closeModal(event) {
     }
   });
 }
+
+function toggleAccordion(event) {
+
+  const accordionHeader = event.target.closest('.accordion__header');
+
+  if (!accordionHeader) return;
+
+  const accordionContainer = accordionHeader.closest('.accordion__container');
+
+  const accordionType = accordionContainer.getAttribute('accordion-type') ?? 'multi';
+
+  const parentElement = accordionHeader.closest('[accordion-id]');
+  const accordionState = parentElement.getAttribute('accordion-state');
+
+  if (accordionType === 'single') {
+    const accordions = accordionContainer.querySelectorAll('[accordion-id]');
+    accordions.forEach((item) => {
+      item.setAttribute('accordion-state', 'closed');
+    });
+  }
+
+  if (accordionState === 'closed') {
+    parentElement.setAttribute('accordion-state', 'open');
+  } else {
+    parentElement.setAttribute('accordion-state', 'closed');
+  }
+}
+
+
+const accordions = document.querySelectorAll('.accordion__container');
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener('click', toggleAccordion);
+});
